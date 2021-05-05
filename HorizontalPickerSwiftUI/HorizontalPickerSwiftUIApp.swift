17c4e1f8c6be45edf5f4934094161346344355f3
Std1o <37378410+Std1o@users.noreply.github.com>
2021-05-05 11:01:22 +0300

    Initial commit

---
+++ HorizontalPickerSwiftUI/HorizontalPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalPickerSwiftUIApp.swift
+//  HorizontalPickerSwiftUI
+//
+//  Created by Илья on 05.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct HorizontalPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
